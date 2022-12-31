e41656f44de488f78336d1c4f3d7a21cd7bdfe37
korigamik <korigamik@gmail.com>
2023-01-01 03:30:23 +0530

    use my data

--- remotion/all.ts
+++ remotion/all.ts
@@ -1,605 +1,113 @@
 // Demo data for the template
+
 export const all = {
-	data: {
-		user: {
-			openIssues: {
-				totalCount: 11,
-			},
-			closedIssues: {
-				totalCount: 58,
-			},
-			avatarUrl: 'https://avatars.githubusercontent.com/u/306134?v=4',
-			login: 'wcandillon',
-			mostRecentPullRequest: {
-				nodes: [
-					{
-						id: 'PR_kwDOGWHiTc5GBMYq',
-						title: 'Fix SkewX and SkewY transform',
-						repository: {
-							name: 'react-native-skia',
-							owner: {
-								login: 'Shopify',
-							},
-						},
-					},
-				],
-			},
-			contributionsCollection: {
-				popularPullRequestContribution: {
-					pullRequest: {
-						id: 'PR_kwDOGWHiTc49lnY0',
-						title: 'Deprecate usePicture in favor of createPicture',
-						repository: {
-							name: 'react-native-skia',
-							owner: {
-								login: 'Shopify',
-							},
-						},
-						totalCommentsCount: 12,
-					},
-				},
-				totalCommitContributions: 870,
-				restrictedContributionsCount: 397,
-				totalIssueContributions: 51,
-				totalRepositoryContributions: 6,
-				totalPullRequestContributions: 373,
-				totalPullRequestReviewContributions: 195,
-				contributionCalendar: {
-					totalContributions: 1913,
-				},
-				commitContributionsByRepository: [
-					{
-						contributions: {
-							totalCount: 646,
-						},
-						repository: {
-							name: 'react-native-skia',
-							owner: {
-								login: 'Shopify',
-							},
-							languages: {
-								edges: [
-									{
-										size: 877610,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-									{
-										size: 566867,
-										node: {
-											color: '#f34b7d',
-											name: 'C++',
-											id: 'MDg6TGFuZ3VhZ2UxNDI=',
-										},
-									},
-									{
-										size: 37719,
-										node: {
-											color: '#b07219',
-											name: 'Java',
-											id: 'MDg6TGFuZ3VhZ2UxNTg=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 67,
-						},
-						repository: {
-							name: 'can-it-be-done-in-react-native',
-							owner: {
-								login: 'wcandillon',
-							},
-							languages: {
-								edges: [
-									{
-										size: 901958,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-									{
-										size: 140202,
-										node: {
-											color: '#f1e05a',
-											name: 'JavaScript',
-											id: 'MDg6TGFuZ3VhZ2UxNDA=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 49,
-						},
-						repository: {
-							name: 'remotion-fireship',
-							owner: {
-								login: 'wcandillon',
-							},
-							languages: {
-								edges: [
-									{
-										size: 116485,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-									{
-										size: 964,
-										node: {
-											color: '#384d54',
-											name: 'Dockerfile',
-											id: 'MDg6TGFuZ3VhZ2U1MzU=',
-										},
-									},
-									{
-										size: 63,
-										node: {
-											color: '#563d7c',
-											name: 'CSS',
-											id: 'MDg6TGFuZ3VhZ2UzMDg=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 34,
-						},
-						repository: {
-							name: 'drawings-and-animations-workshop',
-							owner: {
-								login: 'software-mansion-labs',
-							},
-							languages: {
-								edges: [
-									{
-										size: 145512,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-									{
-										size: 156,
-										node: {
-											color: '#f1e05a',
-											name: 'JavaScript',
-											id: 'MDg6TGFuZ3VhZ2UxNDA=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 29,
-						},
-						repository: {
-							name: 'remotion-skia-tutorial',
-							owner: {
-								login: 'wcandillon',
-							},
-							languages: {
-								edges: [
-									{
-										size: 18103,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 14,
-						},
-						repository: {
-							name: 'xqlint',
-							owner: {
-								login: 'wcandillon',
-							},
-							languages: {
-								edges: [
-									{
-										size: 6271517,
-										node: {
-											color: '#f1e05a',
-											name: 'JavaScript',
-											id: 'MDg6TGFuZ3VhZ2UxNDA=',
-										},
-									},
-									{
-										size: 1303595,
-										node: {
-											color: '#5232e7',
-											name: 'XQuery',
-											id: 'MDg6TGFuZ3VhZ2UyNDE=',
-										},
-									},
-									{
-										size: 68981,
-										node: {
-											color: '#c7254e',
-											name: 'jq',
-											id: 'MDg6TGFuZ3VhZ2U3ODg=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 6,
-						},
-						repository: {
-							name: 'template-skia',
-							owner: {
-								login: 'remotion-dev',
-							},
-							languages: {
-								edges: [
-									{
-										size: 15452,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 6,
-						},
-						repository: {
-							name: 'react-native-redash',
-							owner: {
-								login: 'wcandillon',
-							},
-							languages: {
-								edges: [
-									{
-										size: 85496,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-									{
-										size: 6400,
-										node: {
-											color: '#f1e05a',
-											name: 'JavaScript',
-											id: 'MDg6TGFuZ3VhZ2UxNDA=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 5,
-						},
-						repository: {
-							name: 'eslint-plugin-reanimated',
-							owner: {
-								login: 'wcandillon',
-							},
-							languages: {
-								edges: [
-									{
-										size: 19552,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-									{
-										size: 528,
-										node: {
-											color: '#f1e05a',
-											name: 'JavaScript',
-											id: 'MDg6TGFuZ3VhZ2UxNDA=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 4,
-						},
-						repository: {
-							name: 'eslint-config-react-native-wcandillon',
-							owner: {
-								login: 'wcandillon',
-							},
-							languages: {
-								edges: [
-									{
-										size: 2015,
-										node: {
-											color: '#f1e05a',
-											name: 'JavaScript',
-											id: 'MDg6TGFuZ3VhZ2UxNDA=',
-										},
-									},
-									{
-										size: 398,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 3,
-						},
-						repository: {
-							name: 'expo-tutorial',
-							owner: {
-								login: 'wcandillon',
-							},
-							languages: {
-								edges: [
-									{
-										size: 25133,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-									{
-										size: 609,
-										node: {
-											color: '#f1e05a',
-											name: 'JavaScript',
-											id: 'MDg6TGFuZ3VhZ2UxNDA=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 2,
-						},
-						repository: {
-							name: 'remotion',
-							owner: {
-								login: 'remotion-dev',
-							},
-							languages: {
-								edges: [
-									{
-										size: 3417066,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-									{
-										size: 77718,
-										node: {
-											color: '#f1e05a',
-											name: 'JavaScript',
-											id: 'MDg6TGFuZ3VhZ2UxNDA=',
-										},
-									},
-									{
-										size: 30232,
-										node: {
-											color: '#563d7c',
-											name: 'CSS',
-											id: 'MDg6TGFuZ3VhZ2UzMDg=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 1,
-						},
-						repository: {
-							name: 'react-native-image-viewer',
-							owner: {
-								login: 'wcandillon',
-							},
-							languages: {
-								edges: [
-									{
-										size: 13515,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-									{
-										size: 107,
-										node: {
-											color: '#f1e05a',
-											name: 'JavaScript',
-											id: 'MDg6TGFuZ3VhZ2UxNDA=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 1,
-						},
-						repository: {
-							name: 'SkiaPlayground',
-							owner: {
-								login: 'colinta',
-							},
-							languages: {
-								edges: [
-									{
-										size: 15305,
-										node: {
-											color: '#b07219',
-											name: 'Java',
-											id: 'MDg6TGFuZ3VhZ2UxNTg=',
-										},
-									},
-									{
-										size: 12470,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-									{
-										size: 6973,
-										node: {
-											color: '#f34b7d',
-											name: 'C++',
-											id: 'MDg6TGFuZ3VhZ2UxNDI=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 1,
-						},
-						repository: {
-							name: 'react-native',
-							owner: {
-								login: 'facebook',
-							},
-							languages: {
-								edges: [
-									{
-										size: 6884033,
-										node: {
-											color: '#f1e05a',
-											name: 'JavaScript',
-											id: 'MDg6TGFuZ3VhZ2UxNDA=',
-										},
-									},
-									{
-										size: 3717822,
-										node: {
-											color: '#b07219',
-											name: 'Java',
-											id: 'MDg6TGFuZ3VhZ2UxNTg=',
-										},
-									},
-									{
-										size: 3428520,
-										node: {
-											color: '#f34b7d',
-											name: 'C++',
-											id: 'MDg6TGFuZ3VhZ2UxNDI=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 1,
-						},
-						repository: {
-							name: 'jsoniq',
-							owner: {
-								login: 'wcandillon',
-							},
-							languages: {
-								edges: [
-									{
-										size: 5746103,
-										node: {
-											color: '#3178c6',
-											name: 'TypeScript',
-											id: 'MDg6TGFuZ3VhZ2UyODc=',
-										},
-									},
-									{
-										size: 3076048,
-										node: {
-											color: '#e34c26',
-											name: 'HTML',
-											id: 'MDg6TGFuZ3VhZ2U0MTc=',
-										},
-									},
-									{
-										size: 1284761,
-										node: {
-											color: '#5232e7',
-											name: 'XQuery',
-											id: 'MDg6TGFuZ3VhZ2UyNDE=',
-										},
-									},
-								],
-							},
-						},
-					},
-					{
-						contributions: {
-							totalCount: 1,
-						},
-						repository: {
-							name: 'awesome-react-renderer',
-							owner: {
-								login: 'chentsulin',
-							},
-							languages: {
-								edges: [],
-							},
-						},
-					},
-				],
-			},
+	"avatar": "https://avatars.githubusercontent.com/u/72932688?u=260a02206cb29c6508ff8876aeb2df6bb190592e&v=4",
+	"contributionCount": 1000,
+	"username": "KorigamiK",
+	"repositoriesContributedTo": [
+		"KorigamiK/korigamik",
+		"KorigamiK/UntitledPSP",
+		"KorigamiK/CSE-Works",
+		"KorigamiK/Semester-3",
+		"Perpetual-Motion-Squad/Annapurna",
+		"KorigamiK/noted",
+		"KorigamiK/Neural-Web",
+		"Butterfly-Lunatics/white-space",
+		"KorigamiK/dotfiles",
+		"KorigamiK/Graphing-SFML",
+		"KorigamiK/media-tools",
+		"KorigamiK/free-guitar-tabs",
+		"ossnsut/ossnsut.github.io",
+		"KorigamiK/kickassanime-downloader",
+		"KorigamiK/BadApple",
+		"KorigamiK/blog",
+		"KorigamiK/Advent-Of-Code",
+		"IsomerX/ditto-dolls",
+		"KorigamiK/timothy",
+		"hyugogirubato/Kamyroll-Python",
+		"KorigamiK/iron-donut",
+		"KorigamiK/test-game",
+		"Grub4K/kamyroll-gui",
+		"KorigamiK/php-fruits",
+		"IridescentRose/QG-Flappy-Bird-PSP"
+	],
+	"topLanguages": [
+		{
+			"color": "#f34b7d",
+			"name": "C++"
 		},
+		{
+			"color": "#3178c6",
+			"name": "TypeScript"
+		},
+		{
+			"color": "#563d7c",
+			"name": "TeX"
+		},
+		{
+			"color": "#e34c26",
+			"name": "HTML"
+		},
+		{
+			"color": "#3D6117",
+			"name": "TeX"
+		},
+		{
+			"color": "#DA3434",
+			"name": "CMake"
+		},
+		{
+			"color": "#3572A5",
+			"name": "Python"
+		},
+		{
+			"color": "#555555",
+			"name": "C"
+		},
+		{
+			"color": "#f1e05a",
+			"name": "JavaScript"
+		},
+		{
+			"color": "#AA6746",
+			"name": "Solidity"
+		},
+		{
+			"color": "#000080",
+			"name": "Lua"
+		},
+		{
+			"color": "#dea584",
+			"name": "Rust"
+		},
+		{
+			"color": "#427819",
+			"name": "Makefile"
+		},
+		{
+			"color": "#C1F12E",
+			"name": "Batchfile"
+		},
+		{
+			"color": "#4F5D95",
+			"name": "PHP"
+		}
+	],
+	"issues": {
+		"closed": 6,
+		"open": 3
 	},
-};
+	"commitCount": 912,
+	"pullRequestCount": 32,
+	"mostRecentPullRequest": {
+		"title": "update README",
+		"repository": "ditto-dolls",
+		"organization": "IsomerX"
+	},
+	"mostPopularPullRequest": {
+		"title": "Add Eigen3",
+		"repository": "psp-packages",
+		"organization": "pspdev",
+		"uniqueId": "PR_kwDOF3hLzM5BV4o7"
+	}
+}
